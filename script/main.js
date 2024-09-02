0d032e4abbe766be78b47e55e7ecf3611cd1cc1e
manikumarancse <113590153+manikumarancse@users.noreply.github.com>
2024-09-02 14:56:38 +0530

    Dark Mode theme Added

--- script/main.js
+++ script/main.js
@@ -23,5 +23,26 @@ function linkAction(){
     const navMenu = document.getElementById('nav_menu')
     navMenu.classList.remove('show')
 }
+ 
+navlink.forEach(n => n.addEventListener('click', linkAction));
 
-navlink.forEach(n => n.addEventListener('click', linkAction))
+let darkmode = localStorage.getItem('darkmode')
+
+
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () =>{
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () =>{
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', null)
+}
+if(darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode != "active" ? enableDarkmode() : disableDarkmode()
+})
